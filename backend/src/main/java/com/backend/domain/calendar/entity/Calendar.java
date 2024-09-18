31c25f11306c661d7b01df18662e4860f20f4768
dongwoo <woomacho@gmail.com>
2024-09-18 20:12:55 +0900

    feat : calendar, diary 연관관계 맵핑, 서비스 로직 작성

--- backend/src/main/java/com/backend/domain/calendar/entity/Calendar.java
+++ backend/src/main/java/com/backend/domain/calendar/entity/Calendar.java
@@ -1,27 +1,32 @@
 package com.backend.domain.calendar.entity;
 
-import java.sql.Date;
+import java.util.List;
+import com.backend.domain.diary.entity.Diary;
+import com.backend.global.common.BaseEntity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
-
+import jakarta.persistence.OneToMany;
+import lombok.Builder;
+import lombok.Getter;
+@Getter
 @Entity
-public class Calendar {
+public class Calendar extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String yearMonth;
+	private String monthYear;
 
-	@Temporal(TemporalType.DATE)
-	private Date createdAt;
-
-	@Temporal(TemporalType.DATE)
-	private Date updatedAt;
-
-	private Boolean isDeleted;
+	@OneToMany(mappedBy = "calendar", orphanRemoval = true)
+	private List<Diary> diary;
+	protected Calendar() {
+	}
+	@Builder
+	private Calendar(String monthYear, List<Diary> diary) {
+		this.monthYear = monthYear;
+		this.diary = diary;
+	}
 }
