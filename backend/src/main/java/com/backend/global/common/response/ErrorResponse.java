dfa920723d2012557a0ee44b7005df87c9c9fc43
dongwoo <woomacho@gmail.com>
2024-09-21 01:44:07 +0900

    feat : ErrorCode, ErrorResponse 기초 구현

---
+++ backend/src/main/java/com/backend/global/common/response/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.backend.global.common.response;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+	private final String code;
+	private final String message;
+	public ErrorResponse(ErrorCode errorCode) {
+		this.code = errorCode.getCode();
+		this.message = errorCode.getMessage();
+	}
+	public static ErrorResponse of(final ErrorCode errorCode) {
+		return new ErrorResponse(errorCode);
+	}
+}
