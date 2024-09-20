d73b6c6d22f802a035f75eb255f0b416a5fcaa2a
gang1241 <gang1241@gmail.com>
2024-09-20 10:35:36 +0900

    feat : Define entities

---
+++ backend/src/main/java/com/backend/domain/chat/entity/Sticker.java
@@ -0,0 +1,43 @@
+package com.backend.domain.chat.entity;
+
+import com.backend.domain.diary.entity.Diary;
+import com.backend.global.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Sticker extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "sticker_id")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "diary_id", nullable = false)
+	private Diary diary;
+
+	@Column(length = 500)
+	private String stickerImageUrl;
+
+	private Integer yPosition;
+
+	private Integer xPosition;
+
+	private Integer width;
+
+	private Integer height;
+
+	private Integer rotate;
+}
--- backend/src/main/java/com/backend/domain/diary/entity/Diary.java
+++ backend/src/main/java/com/backend/domain/diary/entity/Diary.java
@@ -1,8 +1,13 @@
 package com.backend.domain.diary.entity;
 
+import java.util.List;
+
 import com.backend.domain.calendar.entity.Calendar;
+import com.backend.domain.chat.entity.Sticker;
+import com.backend.domain.chat.entity.TextBox;
 import com.backend.global.common.BaseEntity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -11,6 +16,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -30,8 +36,15 @@ public class Diary extends BaseEntity {
 	@JoinColumn(name = "calendar_id", nullable = false)
 	private Calendar calendar;
 
+	@OneToMany(mappedBy = "diary", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<TextBox> textBoxes;
+
+	@OneToMany(mappedBy = "diary", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Sticker> stickers;
+
 	protected Diary() {
 	}
+
 	@Builder
 	private Diary(String monthYear, String day, String snsLink, Long diaryBgId, Boolean isExpiry, Calendar calendar) {
 		this.monthYear = monthYear;
@@ -41,6 +54,7 @@ public class Diary extends BaseEntity {
 		this.isExpiry = isExpiry;
 		this.calendar = calendar;
 	}
+
 	public void setIsExpiry(Boolean isExpiry) {
 		this.isExpiry = isExpiry;
 	}
