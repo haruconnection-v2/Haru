9a019df2d753d0f4b7900db990e766810d2828e8
dongwoo <woomacho@gmail.com>
2024-09-21 01:48:14 +0900

    feat : Exception 설정(Handler, Business, BadRequest, Conflict, NotFount)

---
+++ backend/src/main/java/com/backend/global/common/exception/ConflictException.java
@@ -0,0 +1,12 @@
+package com.backend.global.common.exception;
+
+import com.backend.global.common.response.ErrorCode;
+
+public class ConflictException extends BusinessException {
+	public ConflictException(ErrorCode errorCode) {
+		super(errorCode.getMessage(), errorCode);
+	}
+	public ConflictException() {
+		super(ErrorCode.BAD_REQUEST);
+	}
+}
---
+++ backend/src/main/java/com/backend/global/common/exception/NotFoundException.java
@@ -0,0 +1,12 @@
+package com.backend.global.common.exception;
+
+import com.backend.global.common.response.ErrorCode;
+
+public class NotFoundException extends BusinessException {
+	public NotFoundException(ErrorCode errorCode) {
+		super(errorCode.getMessage(), errorCode);
+	}
+	public NotFoundException() {
+		super(ErrorCode.NOT_FOUND);
+	}
+}
