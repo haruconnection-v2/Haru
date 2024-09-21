52350076882f1c077f2b043b9659f7a49eabe980
dongwoo <woomacho@gmail.com>
2024-09-21 19:29:36 +0900

    refactor : Diary Entity 제약사항 추가

--- backend/src/main/java/com/backend/domain/diary/entity/Diary.java
+++ backend/src/main/java/com/backend/domain/diary/entity/Diary.java
@@ -11,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -20,12 +21,17 @@ public class Diary extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(nullable = false)
+	@Pattern(regexp = "^\\d{4}-(0\\d|1[0-2])$", message = "YYYY-MM 형식이 아닙니다.")
 	private String monthYear;
+	@Column(nullable = false)
+	@Pattern(regexp = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01])$", message = "YYYY-MM-DD 형식이 아닙니다.")
 	private String day;
 	private String snsLink;
+	@Column(nullable = false)
 	private Long diaryBgId;
 	@Column(columnDefinition = "BOOLEAN DEFAULT FALSE")
-	private Boolean isExpiry;
+	private boolean isExpiry;
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "calendar_id", nullable = false)
 	private Calendar calendar;
@@ -33,7 +39,7 @@ public class Diary extends BaseEntity {
 	protected Diary() {
 	}
 	@Builder
-	private Diary(String monthYear, String day, String snsLink, Long diaryBgId, Boolean isExpiry, Calendar calendar) {
+	private Diary(String monthYear, String day, String snsLink, Long diaryBgId, boolean isExpiry, Calendar calendar) {
 		this.monthYear = monthYear;
 		this.day = day;
 		this.snsLink = snsLink;
@@ -41,7 +47,11 @@ public class Diary extends BaseEntity {
 		this.isExpiry = isExpiry;
 		this.calendar = calendar;
 	}
-	public void setIsExpiry(Boolean isExpiry) {
-		this.isExpiry = isExpiry;
+
+	public void expireDiary() {
+		if (this.isExpiry) {
+			throw new IllegalStateException("이미 만료된 다이어리입니다.");
+		}
+		this.isExpiry = true;
 	}
 }
