a8bda160200560923e61ce511ff943f0399b9c64
dongwoo <woomacho@gmail.com>
2024-09-24 03:22:55 +0900

    file : Result와 에러 Code, Response 추가된 도메인 적용

--- backend/src/main/java/com/backend/global/common/response/ResultResponse.java
+++ backend/src/main/java/com/backend/global/common/response/ResultResponse.java
@@ -1,25 +1,28 @@
 package com.backend.global.common.response;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Getter;
 
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResultResponse<T> {
 	private final String code;
 	private final String message;
 	private T data;
 
-	@JsonCreator
 	public ResultResponse(final ResultCode resultCode) {
 		this.code = resultCode.getCode();
 		this.message = resultCode.getMessage();
 	}
-	@JsonCreator
 	public ResultResponse(final ResultCode resultCode, T data) {
 		this.code = resultCode.getCode();
 		this.message = resultCode.getMessage();
 		this.data = data;
+	}
+
+	public static <T> ResultResponse<T> of(final ResultCode resultCode) {
+		return new ResultResponse<>(resultCode);
 	}
 
 	public static <T> ResultResponse<T> of(final ResultCode resultCode, final T data) {
